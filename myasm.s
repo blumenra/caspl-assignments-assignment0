453cbde0e05deccd2733da1cdaaae699aaee043a
Alon Blumenreich <alon@mail.com>
2017-03-31 23:11:34 +0300

    Changed the ascii presentations from numbers to the charachters themselves

--- myasm.s
+++ myasm.s
@@ -16,32 +16,32 @@ do_Str:                        	; functions are defined as labels
 	mov	dword [an], 0		; initialize answer
     label_here:
 
-        cmp byte [ecx], 28h     ; cheack if '('
+        cmp byte [ecx], '('     ; cheack if '('
         je conv_left_bracket    ; if '(' go change it into '{'
 
-        cmp byte [ecx], 29h     ; cheack if ')'
+        cmp byte [ecx], ')'     ; cheack if ')'
         je conv_right_bracket   ; if ')' go change it into '}'
 
         jmp first_check         ; it is neither '(' not ')'
         
 
     first_check:
-        cmp byte [ecx], 41h     ; cheack if greater or eq to 'A' (0x41)
+        cmp byte [ecx], 'A'     ; cheack if greater or eq to 'A' (0x41)
         jae second_check        ; if greater or eq to 'A', go check if it is greater or smaller than 'Z'
         jmp non_char            ; else (smaller than 'A') inc the non-char charachters at non-char label
 
     second_check:
-        cmp byte [ecx], 5bh     ; cheack if greater or eq to 'Z' (0x5b)
+        cmp byte [ecx], 'Z'     ; cheack if greater or eq to 'Z' (0x5b)
         jae third_check         ; if greater or eq to 'Z', go check if it is greater or smaller than 'a'
         jmp before_loop_again   ; else (smaller than 'Z') DONT inc the non-char charachters
 
     third_check:
-        cmp byte [ecx], 61h     ; cheack if greater or eq to 'a' (0x61)
+        cmp byte [ecx], 'a'     ; cheack if greater or eq to 'a' (0x61)
         jae forth_check         ; if greater or eq to 'a', go check if it is greater or smaller than 'z'
         jmp non_char            ; else (smaller than 'a') inc the non-char charachters at non-char label because it is a char btween 0x5b~0x60
 
     forth_check:
-        cmp byte [ecx], 7ah     ; cheack if !SMALLER! or eq to 'z' (0x7a)
+        cmp byte [ecx], 'z'     ; cheack if !SMALLER! or eq to 'z' (0x7a)
         jbe conv_to_upper_case  ; if !SMALLER! or eq to 'z', go convert this lower-case char into an upper-case char
         jmp non_char            ; else (greater than 'z') inc the non-char charachters at non-char label because it is a char btween 0x7b~0x7f
 
@@ -52,12 +52,12 @@ do_Str:                        	; functions are defined as labels
 
 
     conv_left_bracket:
-        mov byte [ecx], 3ch
+        mov byte [ecx], '{'
         jmp non_char
 
 
     conv_right_bracket:
-        mov byte [ecx], 3eh
+        mov byte [ecx], '}'
 
 
     non_char:
